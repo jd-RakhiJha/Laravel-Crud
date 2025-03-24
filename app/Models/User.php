d29cbc6dc7f0a75d4d14c3385b51dd263daf0321
Your Name <your-email@example.com>
2025-03-25 02:18:27 +0530

    Today's practice: Working on the frontend and completing the setup for authentication (login/signup).

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -9,7 +11,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Spatie\LaravelData\WithData;
 
-class User extends Model
+class User extends Authenticatable
 {
     use HasFactory, WithData;
 
